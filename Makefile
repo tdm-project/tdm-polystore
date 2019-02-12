00cba96db1d1ef72293443fda65bfca106c6ddef
Gianluigi Zanetti <zag@crs4.it>
2019-02-12 16:33:27 +0100

    All images build and docker-compose is happy.
    Doing docker-compose up brings up an apparently working system.

--- Makefile
+++ Makefile
@@ -1,6 +1,11 @@
 PSWD=foobar
 #IMAGE=timescale/timescaledb
 IMAGE=timescale/timescaledb-postgis
+
+images:
+	docker build -f docker/Dockerfile.hdfs -t tdm/hdfs docker
+	docker build -f docker/Dockerfile.tiledb -t tdm/tiledb docker
+
 run:
 	docker run -d --name timescaledb \
                    -p 5432:5432 -e POSTGRES_PASSWORD=${PSWD} ${IMAGE}
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -4,9 +4,6 @@ set -euo pipefail
 [ -n "${DEBUG:-}" ] && set -x
 
 function onshutdown {
-#    /opt/hadoop/sbin/mr-jobhistory-daemon.sh stop historyserver
-#    /opt/hadoop/sbin/yarn-daemon.sh stop nodemanager
-#    /opt/hadoop/sbin/yarn-daemon.sh stop resourcemanager
     /opt/hadoop/sbin/hadoop-daemon.sh stop datanode
     /opt/hadoop/sbin/hadoop-daemon.sh stop namenode
 }
@@ -20,14 +17,11 @@ export USER="${USER:-$(whoami)}"
 # allow HDFS access from outside the container
 sed -i s/localhost/${HOSTNAME}/ /opt/hadoop/etc/hadoop/core-site.xml
 
-/opt/hadoop/bin/hadoop namenode -format -force
+ /opt/hadoop/bin/hadoop namenode -format -force
 /opt/hadoop/sbin/hadoop-daemon.sh start namenode
 /opt/hadoop/sbin/hadoop-daemon.sh start datanode
 timeout 10 bash -c -- '/opt/hadoop/bin/hdfs dfsadmin -safemode wait' || \
     /opt/hadoop/bin/hdfs dfsadmin -safemode leave
-# /opt/hadoop/sbin/yarn-daemon.sh start resourcemanager
-# /opt/hadoop/sbin/yarn-daemon.sh start nodemanager
-# /opt/hadoop/sbin/mr-jobhistory-daemon.sh start historyserver
 
 tail -f /dev/null
 
