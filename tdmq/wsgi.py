5f7d8d6f5d3334f69f6c60c94c9eab15a0aa5ae2
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2019-05-10 09:02:51 +0200

    Added api version prefix.

--- tdmq/wsgi.py
+++ tdmq/wsgi.py
@@ -1,7 +1,26 @@
 from tdmq import create_app
 
+
+class PrefixMiddleware(object):
+
+    def __init__(self, app, prefix=''):
+        self.app = app
+        self.prefix = prefix
+
+    def __call__(self, environ, start_response):
+
+        if environ['PATH_INFO'].startswith(self.prefix):
+            environ['PATH_INFO'] = environ['PATH_INFO'][len(self.prefix):]
+            environ['SCRIPT_NAME'] = self.prefix
+            return self.app(environ, start_response)
+        else:
+            start_response('404', [('Content-Type', 'text/plain')])
+            return ["This url does not belong to the app.".encode()]
+
+
 app = create_app()
+app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix='/api/v0.0')
+
 
 if __name__ == "__main__":
     app.run()
-
