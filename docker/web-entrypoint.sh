b3b555998fea6496b7152ed448206fdefa121e91
Mauro Del Rio <mauro@crs4.it>
2019-06-17 14:26:50 +0200

    adds environment variable for controlling db initialization

--- docker/web-entrypoint.sh
+++ docker/web-entrypoint.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
+create_db() {
 db_created=0
-for i in $(seq 1 10); do
-  sleep 5
-  echo "trying to init db..."
-  flask db init
-  if [ "$?" -eq 0 ]; then
-    db_created=1
-    echo "db initialized."
-    break
+  for i in $(seq 1 10); do
+    sleep 5
+    echo "trying to init db..."
+    flask db init
+    if [ "$?" -eq 0 ]; then
+      db_created=1
+      echo "db initialized."
+      break
+    fi
+  done
+  if [ "$db_created" -ne 1 ]; then
+    echo "cannot initialize db... exiting."
+    exit 1
   fi
-done
-if [ "$db_created" -ne 1 ]; then
-  echo "cannot initialize db... exiting."
-  exit 1
+}
+
+CREATE_DB=${CREATE_DB:-false}
+if $CREATE_DB ; then
+  create_db
 fi
+
 gunicorn -b 0.0.0.0:8000 wsgi:app
