63c63572eb621317f1be3ca7f637bea53d0d67fa
simleo <simleo@crs4.it>
2019-06-26 12:34:10 +0200

    update unit tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,7 +1,8 @@
 import json
 import pytest
-from collections import Counter
+import uuid
 from collections import OrderedDict
+from copy import deepcopy
 from datetime import datetime, timedelta
 from tdmq.utils import convert_footprint
 
@@ -15,23 +16,29 @@ measures_fname = os.path.join(root, 'data/measures.json')
 # FIXME move it to a fixture?
 class FakeDB:
 
+    NS = uuid.UUID('6cb10168-c65b-48fa-af9b-a3ca6d03156d')
+
+    def __load(self, fname):
+        table = OrderedDict()
+        with open(fname) as f:
+            data = json.load(f)
+        descriptions = next(iter(data.values()))
+        for d in descriptions:
+            code = str(uuid.uuid5(self.NS, d['name']))
+            table[code] = d
+        return table
+
     def __init__(self):
         self.initialized = False
         self.called = {}
-        sensor_types = json.load(
-            open(os.path.join(root, 'data/sensor_types.json')))['sensor_types']
-        sensors = json.load(
-            open(os.path.join(root, './data/sensors.json')))['sensors']
-        self.sensor_types = OrderedDict((_['code'], _) for _ in sensor_types)
-        self.sensors = OrderedDict((_['code'], _) for _ in sensors)
-        self.sensors_no_args = dict(Counter(
-            _['stypecode'] for _ in self.sensors.values()
-        ))
+        self.sensor_types = self.__load(sensor_types_fname)
+        self.sensors = self.__load(sensors_fname)
         measures = json.load(
             open(os.path.join(root, 'data/measures.json')))['measures']
         data = {}
         for m in measures:
-            data.setdefault(m['sensorcode'], []).append(
+            sensorcode = str(uuid.uuid5(self.NS, m['sensor']))
+            data.setdefault(sensorcode, []).append(
                 (datetime.strptime(m['time'], '%Y-%m-%dT%H:%M:%SZ'),
                  m['measure']['value']))
         self.timeseries = {}
@@ -48,18 +55,15 @@ class FakeDB:
 
     def list_sensor_types(self, args):
         self.called['list_sensor_types'] = args
-        return [_ for _ in self.sensor_types.values()]
+        return list(deepcopy(self.sensor_types).items())
 
     def list_sensors(self, args):
         self.called['list_sensors'] = args
-        if args:
-            return [_ for _ in self.sensors.values()]
-        else:
-            return self.sensors_no_args
+        return list(deepcopy(self.sensors).items())
 
     def get_sensor(self, code):
         self.called['get_sensor'] = {'code': code}
-        return self.sensors[code]
+        return deepcopy(self.sensors[code])
 
     def get_timeseries(self, code, args):
         args['code'] = code
@@ -67,14 +71,24 @@ class FakeDB:
         return self.timeseries[code]
 
 
+def _checkresp(response, table=None):
+    assert response.status == '200 OK'
+    assert response.is_json
+    if table:
+        result = response.get_json()
+        assert len(result) == len(table)
+        for r in result:
+            assert "code" in r
+            code = r.pop("code")
+            assert r == table[code]
+
+
 def test_sensor_types_no_args(client, monkeypatch):
     fakedb = FakeDB()
     monkeypatch.setattr('tdmq.db.list_sensor_types', fakedb.list_sensor_types)
     response = client.get('/sensor_types')
     assert 'list_sensor_types' in fakedb.called
-    assert response.status == '200 OK'
-    assert response.is_json
-    assert response.get_json() == fakedb.list_sensor_types({})
+    _checkresp(response, table=fakedb.sensor_types)
 
 
 def test_sensor_types(client, monkeypatch):
@@ -86,9 +100,7 @@ def test_sensor_types(client, monkeypatch):
     assert 'list_sensor_types' in fakedb.called
     args = fakedb.called['list_sensor_types']
     assert {k: v for k, v in args.items()} == in_args
-    assert response.status == '200 OK'
-    assert response.is_json
-    assert response.get_json() == fakedb.list_sensor_types(args)
+    _checkresp(response, table=fakedb.sensor_types)
 
 
 def test_sensors_no_args(client, monkeypatch):
@@ -96,9 +108,7 @@ def test_sensors_no_args(client, monkeypatch):
     monkeypatch.setattr('tdmq.db.list_sensors', fakedb.list_sensors)
     response = client.get('/sensors')
     assert 'list_sensors' in fakedb.called
-    assert response.status == '200 OK'
-    assert response.is_json
-    assert response.get_json() == fakedb.sensors_no_args
+    _checkresp(response, table=fakedb.sensors)
 
 
 def test_sensors(client, monkeypatch):
@@ -106,18 +116,13 @@ def test_sensors(client, monkeypatch):
     monkeypatch.setattr('tdmq.db.list_sensors', fakedb.list_sensors)
     footprint = 'circle((9.2, 33), 1000)'
     after, before = '2019-02-21T11:03:25Z', '2019-02-21T11:50:25Z'
-    type_ = next(iter(fakedb.sensor_types))
-    q = 'footprint={}&after={}&before={}&type={}'.format(
-        footprint, after, before, type_)
+    q = 'footprint={}&after={}&before={}'.format(footprint, after, before)
     response = client.get('/sensors?{}'.format(q))
     assert 'list_sensors' in fakedb.called
-    assert response.status == '200 OK'
-    assert response.is_json
     args = fakedb.called['list_sensors']
     assert args['footprint'] == convert_footprint(footprint)
     assert args['after'] == after and args['before'] == before
-    assert args['type'] == type_
-    assert response.get_json() == fakedb.list_sensors(args)
+    _checkresp(response, table=fakedb.sensors)
 
 
 def test_sensors_fail(client, monkeypatch):
@@ -139,12 +144,14 @@ def test_sensor(client, monkeypatch):
     monkeypatch.setattr('tdmq.db.get_sensor', fakedb.get_sensor)
     code = list(fakedb.sensors.keys())[0]
     response = client.get('/sensors/{}'.format(code))
-    assert 'get_sensor' in fakedb.called
-    assert response.status == '200 OK'
-    assert response.is_json
     args = fakedb.called['get_sensor']
     assert args['code'] == code
-    assert response.get_json() == fakedb.get_sensor(code)
+    assert 'get_sensor' in fakedb.called
+    _checkresp(response)
+    result = response.get_json()
+    assert "code" in result
+    code = result.pop("code")
+    assert result == fakedb.sensors[code]
 
 
 def test_timeseries(client, monkeypatch):
--- tests/test_db_query.py
+++ tests/test_db_query.py
@@ -11,12 +11,16 @@ from tdmq.db import list_sensors_in_db
 root = os.path.dirname(os.path.abspath(__file__))
 
 
+def _getdesc(query_result):
+    return [description for code, description in query_result]
+
+
 def test_db_list_sensor_types(db):
     sensor_types = json.load(
         open(os.path.join(root, 'data/sensor_types.json')))['sensor_types']
     data = list_descriptions_in_table(db, 'sensor_types')
     assert len(sensor_types) == len(data)
-    assert data == sensor_types
+    assert _getdesc(data) == sensor_types
 
 
 def test_list_sensor_types_with_args(db):
@@ -27,11 +31,11 @@ def test_list_sensor_types_with_args(db):
         "temperature" in _["controlledProperty"],
         _["manufacturerName"] == "CRS4"
     ))]
-    assert list_sensor_types_in_db(db, args) == exp_res
+    assert _getdesc(list_sensor_types_in_db(db, args)) == exp_res
     args = {"controlledProperty": "temperature,humidity"}
     exp_res = [_ for _ in sensor_types if
                {"temperature", "humidity"}.issubset(_["controlledProperty"])]
-    assert list_sensor_types_in_db(db, args) == exp_res
+    assert _getdesc(list_sensor_types_in_db(db, args)) == exp_res
 
 
 def test_list_sensors(db):
@@ -39,36 +43,38 @@ def test_list_sensors(db):
         open(os.path.join(root, 'data/sensors.json')))['sensors']
     data = list_descriptions_in_table(db, 'sensors')
     assert len(sensors) == len(data)
-    assert data == sensors
+    assert _getdesc(data) == sensors
 
 
 def test_db_get_sensor(db):
     sensors = json.load(
         open(os.path.join(root, 'data/sensors.json')))['sensors']
     for s in sensors:
-        assert s == get_object(db, 'sensors', s['code'])
+        code = list_sensors_in_db(db, {"name": s["name"]})[0][0]
+        assert s == get_object(db, 'sensors', code)
 
 
 def test_db_get_sensor_type(db):
     sensor_types = json.load(
         open(os.path.join(root, 'data/sensor_types.json')))['sensor_types']
     for s in sensor_types:
-        assert s == get_object(db, 'sensor_types', s['code'])
+        code = list_sensor_types_in_db(db, {"name": s["name"]})[0][0]
+        assert s == get_object(db, 'sensor_types', code)
 
 
 def test_list_sensors_with_args(db):
     """\
     Sensor dist from [9.2215, 30.0015] (coord conversion with GDAL):
-      "0fd67c67-c9be-45c6-9719-4c4eada4beff" 173.07071847340288
-      "0fd67c67-c9be-45c6-9719-4c4eada4becc" 173.0708798144605
-      "838407d9-9876-4226-a039-ff17ba833b2c" 44533.32174325444
-      "d5307bae-76a9-4298-885c-05e7a4d521c2" 111412.41515372833
-      "13c1cb32-486a-407f-b286-dc9ea8fef99f" 133329.30383038227
-      "1f69d31c-a5ef-4ef4-902d-45f5e57923c6" 223714.63917666752
+      "sensor_1" 173.07071847340288
+      "sensor_0" 173.0708798144605
+      "sensor_2" 44533.32174325444
+      "sensor_5" 111412.41515372833
+      "sensor_4" 133329.30383038227
+      "sensor_3" 223714.63917666752
     """
     sensors = json.load(
         open(os.path.join(root, 'data/sensors.json')))['sensors']
-    sensors_by_code = dict((s['code'], s) for s in sensors)
+    sensors_by_name = dict((s['name'], s) for s in sensors)
     center = [9.2215, 30.0015]
     expected_sensors_by_radius = {
         175: 2, 45000: 3, 115000: 4, 135000: 5, 225000: 6
@@ -83,17 +89,14 @@ def test_list_sensors_with_args(db):
         }
         data = list_sensors_in_db(db, args)
         assert len(data) == exp_n
-        for d in data:
-            assert d['code'] in sensors_by_code
-            assert d['stypecode'] == sensors_by_code[d['code']]['stypecode']
-    # query by type
-    t = sensors[0]["stypecode"]
-    exp_res = [_ for _ in sensors if _["stypecode"] == t]
+        for _, d in data:
+            assert d['name'] in sensors_by_name
+            assert sensors_by_name[d['name']] == d
+    # query by attribute
+    t = sensors[0]["type"]
+    exp_res = [_ for _ in sensors if _["type"] == t]
     res = list_sensors_in_db(db, {"type": t})
-    assert res == exp_res
-
-    res = list_sensors_in_db(db, {"name": "test"})
-    assert len(res) == 1
+    assert _getdesc(res) == exp_res
 
 
 def test_get_scalar_timeseries_data(db):
@@ -109,33 +112,35 @@ def test_get_scalar_timeseries_data(db):
         open(os.path.join(root, 'data/sensors.json')))['sensors']
     measures = json.load(
         open(os.path.join(root, 'data/measures.json')))['measures']
-    sensors_by_code = dict((s['code'], s) for s in sensors)
+    sensors_by_name = dict((s['name'], s) for s in sensors)
     timebase = to_dt(args['after'])
-    deltas_by_code = {}
-    values_by_code = {}
+    deltas_by_name = {}
+    values_by_name = {}
     for m in measures:
-        deltas_by_code.setdefault(m['sensorcode'], []).append(
+        deltas_by_name.setdefault(m['sensor'], []).append(
             (to_dt(m['time']) - timebase).total_seconds())
-        values_by_code.setdefault(m['sensorcode'], []).append(
+        values_by_name.setdefault(m['sensor'], []).append(
             m['measure']['value'])
-    for code in sensors_by_code:
+    for name in sensors_by_name:
+        code = list_sensors_in_db(db, {"name": name})[0][0]
         args['code'] = code
         result = get_scalar_timeseries_data(db, args)
         assert result['timebase'] == args['after']
-        assert result['timedelta'] == deltas_by_code.get(code, [])
-        assert result['data'] == values_by_code.get(code, [])
+        assert result['timedelta'] == deltas_by_name.get(name, [])
+        assert result['data'] == values_by_name.get(name, [])
 
 
 def test_get_scalar_timeseries_data_empty(db):
     sensors = json.load(
         open(os.path.join(root, 'data/sensors.json')))['sensors']
-    sensors_by_code = dict((s['code'], s) for s in sensors)
+    sensors_by_name = dict((s['name'], s) for s in sensors)
     args = {}
     args['bucket'] = timedelta(seconds=3)
     args['op'] = 'sum'
     args['after'] = '2010-05-02T11:00:00Z'
     args['before'] = '2010-05-02T11:50:25Z'
-    for code in sensors_by_code:
+    for name in sensors_by_name:
+        code = list_sensors_in_db(db, {"name": name})[0][0]
         args['code'] = code
         result = get_scalar_timeseries_data(db, args)
         assert result['timebase'] == args['after']
