f3a88ca76e74724a8bacd7a81ab09f9ff9a19d36
Gianluigi Zanetti <gianluigi.zanetti@crs4.it>
2019-07-26 17:14:02 +0200

    Updated rest api to the new data model.

--- tdmq/api.py
+++ tdmq/api.py
@@ -12,24 +12,27 @@ def add_routes(app):
     @app.route('/')
     def index():
         return 'The URL for this page is {}'.format(url_for('index'))
-    @app.route('/sensors', methods=['GET', 'POST'])
 
-    def sensors():
-        """Return a list of sensors.
+    @app.route('/sources', methods=['GET', 'POST'])
+    def sources():
+        """Return a list of sources.
 
-        .. :quickref: Get sensors
+        .. :quickref: Get sources
 
-        With no parameters, return all sensors. When ``footprint``, ``after``
-        and ``before`` are specified, return all sensors that have reported an
-        event in the corresponding spatio-temporal region. Sensors can also be
+        With no parameters, return all sources. When ``footprint``, ``after``
+        and ``before`` are specified, return all sources that have reported an
+        event in the corresponding spatio-temporal region. Sources can also be
         filtered by generic attributes stored in the description field.
 
-        Note: currently queries by footprint or by attributes are mutually
-        exclusive, i.e. they cannot be combined in a single query.
+        The footprint should be specified using one of the following:
+         - circle((center_lon, center_lat), radius_in_meters)
+         - FIXME: rectangle?
+         - FIXME: arbitrary GeoJson?
+
 
         **Example request**::
 
-          GET /sensors?footprint=circle((9.22, 30.0), 1000)
+          GET /sources?footprint=circle((9.22, 30.0), 1000)
                       &after=2019-05-02T11:00:00Z
                       &before=2019-05-02T11:50:25Z HTTP/1.1
 
@@ -42,58 +45,55 @@ def add_routes(app):
           HTTP/1.1 200 OK
           Content-Type: application/json
 
-          [{"code": "c034f147-8e54-50bd-97bb-9db1addcdc5a",
-            "geometry": {"coordinates": [9.221, 30.0], "type": "Point"},
-            "name": "sensor_0",
-            "node": "node_0",
-            "type": "sensor_type_0"},
-           {"code": "c932ff51-6eec-5b73-abe1-4163f9e72cb3",
-            "geometry": {"coordinates": [9.222, 30.003], "type": "Point"},
-            "name": "sensor_1",
-            "node": "node_0",
-            "type": "sensor_type_1"}]
+          [{"tdmq_id": "c034f147-8e54-50bd-97bb-9db1addcdc5a",
+            "id": "source_0",
+            "geometry_ype": "Point",
+            "entity_type": "entity_type0},
+           {"tdmq_id": "c034f147-8e54-50bd-97bb-9db1addcdc5b",
+            "id": "source_1",
+            "geometry_ype": "Point",
+            "entity_type": "entity_type_1"}]
 
         :resheader Content-Type: application/json
 
-        :query footprint: consider only sensors within footprint
+        :query footprint: consider only sources within footprint
           e.g., ``circle((9.3, 32), 1000)``
 
-        :query after: consider only sensors reporting  after (included)
+        :query after: consider only sources reporting  after (included)
           this time, e.g., ``2019-02-21T11:03:25Z``
 
-        :query before: consider only sensors reporting strictly before
+        :query before: consider only sources reporting strictly before
           this time, e.g., ``2019-02-22T11:03:25Z``
 
-        :query {attribute}: select sensors whose description has the
+        :query {attribute}: select sources whose description has the
             specified value(s) for the chosen attribute
-            (top-level JSON key, e.g., type=sensor_type_1)
-
+            (top-level JSON key, e.g., controlledProperty=temperature)
         :status 200: no error
-        :returns: list of sensors
+        :returns: list of sources
         """
         if request.method == "GET":
             args = {k: v for k, v in request.args.items()}
             if 'footprint' in args:
                 args['footprint'] = convert_footprint(args['footprint'])
             res = []
-            for code, descr in db.list_sensors(args):
-                descr["code"] = code
+            for tdmq_id, descr in db.list_sources(args):
+                descr["tdmq_id"] = tdmq_id
                 res.append(descr)
             return jsonify(res)
         else:
             data = request.json
-            codes = db.load_sensors(db.get_db(), data)
-            return jsonify(codes)
+            tdmq_ids = db.load_sources(db.get_db(), data)
+            return jsonify(tdmq_ids)
 
-    @app.route('/sensors/<uuid:code>')
-    def sensor(code):
-        """Return description of sensor with uuid ``code``.
+    @app.route('/sources/<uuid:tdmq_id>')
+    def source(tdmq_id):
+        """Return description of source with uuid ``tdmq_id``.
 
-        .. :quickref: Get sensor description
+        .. :quickref: Get source description
 
         **Example request**::
 
-          GET /sensors/0fd67c67-c9be-45c6-9719-4c4eada4becc HTTP/1.1
+          GET /sources/0fd67c67-c9be-45c6-9719-4c4eada4becc HTTP/1.1
 
         **Example response**:
 
@@ -102,33 +102,32 @@ def add_routes(app):
           HTTP/1.1 200 OK
           Content-Type: application/json
 
-          {"code": "c034f147-8e54-50bd-97bb-9db1addcdc5a",
-           "geometry": {"coordinates": [9.221, 30.0], "type": "Point"},
-           "name": "sensor_0",
-           "node": "node_0",
-           "type": "sensor_type_0"}
+          {"tdmq_id": "c034f147-8e54-50bd-97bb-9db1addcdc5a",
+           "id": "source_0",
+           "geometry_ype": "Point",
+           "entity_type": "entity_type_0"}
 
         :resheader Content-Type: application/json
         :status 200: no error
-        :returns: sensor description
+        :returns: source description
         """
-        res = db.get_sensor(str(code))
-        res["code"] = code
+        res = db.get_source(str(tdmq_id))
+        res["tdmq_id"] = tdmq_id
         return jsonify(res)
 
-    @app.route('/sensors/<uuid:code>/timeseries')
-    def timeseries(code):
-        """Return timeseries for sensor ``code``.
+    @app.route('/sources/<uuid:tdmq_id>/timeseries')
+    def timeseries(tdmq_id):
+        """Return timeseries for source ``tdmq_id``.
 
-        .. :quickref: Get time series data for sensor
+        .. :quickref: Get time series data for source
 
-        For the specified sensor and time interval, return all measures and
+        For the specified source and time interval, return all records and
         the corresponding timedeltas array (expressed in seconds from the
         initial time). Also returns the initial time as "timebase".
 
         **Example request**::
 
-          GET /sensors/0fd67c67-c9be-45c6-9719-4c4eada4becc/
+          GET /sources/0fd67c67-c9be-45c6-9719-4c4eada4becc/
               timeseries?after=2019-02-21T11:03:25Z
                         &before=2019-05-02T11:50:25Z HTTP/1.1
 
@@ -141,14 +140,14 @@ def add_routes(app):
 
           {"timebase": "2019-02-21T11:03:25Z",
            "timedelta": [0.11, 0.22, 0.33, 0.44],
-           "data": [12000, 12100, 12200, 12300]}
+           "data": {'temperature':[12000, 12100, 12200, 12300]}}
 
         :resheader Content-Type: application/json
 
-        :query after: consider only sensors reporting after (included)
+        :query after: consider only sources reporting after (included)
                       this time, e.g., '2019-02-21T11:03:25Z'
 
-        :query before: consider only sensors reporting strictly before
+        :query before: consider only sources reporting strictly before
                       this time, e.g., '2019-02-22T11:03:25Z'
 
         :query bucket: time bucket for data aggregation, in seconds,
@@ -158,7 +157,7 @@ def add_routes(app):
                    e.g., `sum`, `count`.
 
         :status 200: no errors
-        :returns: list of sensors
+        :returns: list of sources
         """
         rargs = request.args
         args = dict((k, rargs.get(k, None))
@@ -167,11 +166,11 @@ def add_routes(app):
             assert args['op'] is not None
             args['bucket'] = timedelta(seconds=float(args['bucket']))
 
-        res = db.get_timeseries(str(code), args)
+        res = db.get_timeseries(str(tdmq_id), args)
         return jsonify(res)
 
-    @app.route('/measures', methods=["POST"])
-    def measures():
+    @app.route('/records', methods=["POST"])
+    def records():
         data = request.json
-        n = db.load_measures(db.get_db(), data)
+        n = db.load_records(db.get_db(), data)
         return jsonify({"loaded": n})
