c3e5e0919cdcadf48963fa2dedbbb5f8df2d2133
Luca Pireddu <pireddu@crs4.it>
2019-08-05 12:34:48 +0200

    Rearrange some docker image layers for better caching during development

---
+++ docker/tdmq_scripts.sh
@@ -0,0 +1,25 @@
+
+
+function fake_user() {
+    # Make the current UID:GID look like the specified username
+    local username="${1}"
+    if [[ -z "${username}" ]]; then
+        printf "Missing argument:  username\n" >&2
+        exit 2
+    fi
+        
+    if ! getent passwd "$(id -u)" &> /dev/null && [ -e /usr/lib/libnss_wrapper.so ]; then
+        local uid=$(id -u)
+        local gid=$(id -g)
+        export LD_PRELOAD='/usr/lib/libnss_wrapper.so'
+        export NSS_WRAPPER_PASSWD="$(mktemp)"
+        export NSS_WRAPPER_GROUP="$(mktemp)"
+        echo "${username}:x:${uid}:${gid}:${username}:$HOME:/bin/false" > "$NSS_WRAPPER_PASSWD"
+        echo "${username}:x:${gid}:" > "$NSS_WRAPPER_GROUP"
+
+        printf "Installed fake NSS user %s with UID %s and GID %s\n" "${username}" ${uid} ${gid} >&2
+        printf "whoami? %s\n" "$(whoami)"
+    else
+        printf "Can't install fake user.  libnss_wrapper is not installed\n" >&2
+    fi
+}
--- docker/tdmqj-entrypoint.sh
+++ docker/tdmqj-entrypoint.sh
@@ -1,12 +1,8 @@
 #!/bin/bash -l
 
+source /usr/local/lib/tdmq_scripts.sh
+
 # Make the current UID:GID look like the `jupyter` user
-if ! getent passwd "$(id -u)" &> /dev/null && [ -e /usr/lib/libnss_wrapper.so ]; then
-  export LD_PRELOAD='/usr/lib/libnss_wrapper.so'
-  export NSS_WRAPPER_PASSWD="$(mktemp)"
-  export NSS_WRAPPER_GROUP="$(mktemp)"
-  echo "jupyter:x:$(id -u):$(id -g):Jupyter:$HOME:/bin/false" > "$NSS_WRAPPER_PASSWD"
-  echo "jupyter:x:$(id -g):" > "$NSS_WRAPPER_GROUP"
-fi
+fake_user "jupyter"
 
 jupyter notebook --ip=0.0.0.0 --port=8888 --no-browser "${@}"
