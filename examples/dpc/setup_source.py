72da9d65f6897fd7d4473f79922da8d82ab6e96d
Mauro Del Rio <mauro@crs4.it>
2020-11-05 11:21:37 +0100

    refactor setup_sorce: use clize instead of argparse, make it works even if no time series were previously created

--- examples/dpc/setup_source.py
+++ examples/dpc/setup_source.py
@@ -1,30 +1,14 @@
-
-import argparse
 import logging
-import numpy as np
-import sys
-
 from datetime import datetime, timedelta
 
-from tdmq.client import Client
+import numpy as np
+from clize import parameters, run
 from dpc import fetch_dpc_data
-from utils import load_desc, create_timebase
+from utils import create_timebase, load_desc
+
+from tdmq.client import Client
 
 logger = None
-
-
-def build_parser():
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--source", dest='source',
-                        choices=['radar', 'temperature'],
-                        help="dpc data source", required=True)
-    parser.add_argument("--tdmq", dest='tdmq', help="tdmq web server address")
-    parser.add_argument("-V", dest='tdmq_version', help="tdmq version", default='0.0')
-    parser.add_argument("--hdfs", dest='hdfs', help="hdfs address")
-    parser.add_argument("--log-level", choices=["DEBUG", "INFO", "WARN", "ERROR", "CRITICAL"],
-                        default="INFO", help="Logging level")
-
-    return parser
 
 
 def setup_logging(level_name):
@@ -36,17 +20,20 @@ def setup_logging(level_name):
     logger.info('Logging is active (level %s).', level_name)
 
 
-def main(args):
-    parser = build_parser()
-    opts = parser.parse_args(args)
-    setup_logging(opts.log_level)
+def main(source: parameters.one_of('radar', 'temperature'),
+         *,
+         tdmq_url: ('u'),
+         log_level: ('v',
+                     parameters.one_of("DEBUG", "INFO", "WARN", "ERROR",
+                                       "CRITICAL")) = 'INFO'):
+    setup_logging(log_level)
 
-    desc = load_desc(opts.source)
+    desc = load_desc(source)
 
     now = datetime.now()
     dt = timedelta(seconds=desc['description']['acquisition_period'])
 
-    c = Client()
+    c = Client(tdmq_url)
     srcs = c.find_sources({'id': desc['id']})
     if len(srcs) > 0:
         assert len(srcs) == 1
@@ -56,11 +43,13 @@ def main(args):
         s = c.register_source(desc)
         logger.info(f"Created source {s.tdmq_id} for {s.id}.")
 
-    ts = s.timeseries()
-
+    try:
+        ts = s.timeseries()
+    except Exception as ex:  # FIXME too general
+        ts = []
     # The DPC source keeps data available for only one week
-    time_base = ts.time[0] \
-        if len(ts) > 0 else create_timebase(now, timedelta(seconds=6 * 24 * 3600))
+    time_base = ts.time[0] if len(ts) > 0 else create_timebase(
+        now, timedelta(seconds=6 * 24 * 3600))
     # It is unlikely that the last frame will be ready, so we stop the
     # request at now - dt.
     times = np.arange(time_base, now - dt, dt)
@@ -80,4 +69,4 @@ def main(args):
 
 
 if __name__ == "__main__":
-    main(sys.argv[1:])
+    run(main)
