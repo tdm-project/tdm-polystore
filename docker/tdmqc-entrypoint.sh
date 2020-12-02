8efb0066bb5c2def95172338c54411c02fce3236
Luca Pireddu <pireddu@crs4.it>
2020-12-03 00:03:15 +0100

    Add possibility to pass arguments to tdmqc entrypoint

--- docker/tdmqc-entrypoint.sh
+++ docker/tdmqc-entrypoint.sh
@@ -2,18 +2,28 @@
 
 export PYTHONPATH="${TDMQ_DIST}"
 
-tdmqc_run_tests
+function run_tests() {
+  tdmqc_run_tests
 
-exit_code=$?
+  exit_code=$?
 
-printf "======================================================================\n" >&2
+  printf "======================================================================\n" >&2
 
-if [[ $exit_code != 0 ]]; then
-	printf "Tests FAILED!\n" >&2
+  if [[ $exit_code != 0 ]]; then
+    printf "Tests FAILED!\n" >&2
+  else
+    printf "Tests ran successfully!\n" >&2
+  fi
+
+  printf "======================================================================\n" >&2
+  return $exit_code
+}
+
+if [[ $# -ge 1 ]]; then
+    printf "Executing command: ${@}\n" >&2
+    exec "${@}"
 else
-	printf "Tests ran successfully!\n" >&2
+    run_tests
+    printf "Hanging around to keep container alive.  Kill me to exit\n" >&2
+    tail -f /dev/null
 fi
-
-printf "======================================================================\n" >&2
-
-tail -f /dev/null
