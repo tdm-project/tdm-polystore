ffa880e6f400101b1465238700d66b7adc5ade23
Luca Pireddu <pireddu@crs4.it>
2021-12-09 22:34:48 +0100

    Change tdmqc container entrypoint not to run tests

--- docker/tdmqc-entrypoint.sh
+++ docker/tdmqc-entrypoint.sh
@@ -1,34 +1,19 @@
 #!/bin/bash
 
-function run_tests() {
-  export PYTHONPATH="${TDMQ_DIST}"
-  tdmqc_run_tests
-
-  exit_code=$?
-
-  printf "======================================================================\n" >&2
-
-  if [[ $exit_code != 0 ]]; then
-    printf "Tests FAILED!\n" >&2
-  else
-    printf "Tests ran successfully!\n" >&2
-  fi
-
-  printf "======================================================================\n" >&2
-  return $exit_code
-}
-
 ## main ##
+if [[ -z "${DONT_UNSET_HADOOP_VARS:-}" ]]; then
+    echo 'Unsetting HADOOP_* env vars' >&2
+    for varname in ${!HADOOP*}; do
+        unset "${varname}"
+    done
+else
+    echo 'The following HADOOP_* variables are set:' "${!HADOOP*}" >&2
+fi
+
 if [[ $# -ge 1 ]]; then
-    if [[ -z "${DONT_UNSET_HADOOP_VARS:-}" ]]; then
-        echo "Unsetting HADOOP_HOME and HADOOP_LOG_DIR env vars" >&2
-        unset HADOOP_HOME
-        unset HADOOP_LOG_DIR
-    fi
     echo "Executing command:" "${@}" >&2
     exec "${@}"
 else
-    run_tests
     printf "Hanging around to keep container alive.  Kill me to exit\n" >&2
     tail -f /dev/null
 fi
