6b7a4e5878e2aabad8bea08aec50ad2b5e865b7c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-09 01:51:51 +0000

    Adding tarfile member sanitization to extractall()

--- tdmq/loc_anonymizer.py
+++ tdmq/loc_anonymizer.py
@@ -79,7 +79,26 @@ def _extract_zonedb_archive(archive_path, dest):
             else:
                 _logger.warning("Skipping item in tar archive:  %s", m.name)
     with tarfile.open(archive_path) as tar:
-        tar.extractall(path=dest, members=safe_members(tar.getmembers()))
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, path=dest, members=safe_members(tar.getmembers()))
 
 
 def _iter_shapefile_archive(uri):
